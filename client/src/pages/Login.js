38fc5dbfbbc88b46206d93c147a155cdb8bec5ef
Emma Perlman <emmajperlman@gmail.com>
2022-11-27 18:44:19 -0800

    create files for Login page, LoginForm and SignupForm

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm'
+import SignupForm from '../components/SignupForm'
+
+const Login = () => {
+  return (
+    <div>
+      <LoginForm></LoginForm>
+      <SignupForm></SignupForm>
+    </div>
+  )
+}
+
+export default Login
