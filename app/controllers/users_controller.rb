3894f85af2b4f8c0d517ed086687d02f81e16c54
Emma Perlman <emmajperlman@gmail.com>
2022-11-27 16:21:40 -0800

    created sign up route and ability to create a user if valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+    def create
+        user=User.create(user_params)
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+    
     def show
         user=User.find_by(id:session[:user_id])
         if user
