05e3a985778ae59f76098067d4a0df5d47095e8e
Emma Perlman <emmajperlman@gmail.com>
2022-12-17 14:49:22 -0800

    initialize products routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
+  #Login,SignUp, Routes
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
+
+  #Products Routes
+  resources :products, only:[:index, :create]
 end
